1d45299eeb183ab677942fd30e5db7906975371c
Xennis <code@xennis.org>
2022-01-23 19:09:10 +0100

    Rename dart package name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lifetime/main.dart';
+import 'package:lifetime_clock/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
